40b47879b8b6254fd5eae1741262e1aa056dab59
Mihir <68944128+Mihir-27@users.noreply.github.com>
2022-08-15 15:44:24 +0000

    Setup & Hero Section

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{njk,md}", "./src/**/*.svg",],
+  theme: {
+    extend: {
+      fontFamily : {
+        head : ["'Space Grotesk', sans-serif"],
+        body : ["'Inter', sans-serif"],
+        extra : ["'JetBrains Mono', monospace"],
+      },
+      colors : {
+        dark: '#0d0d0d',
+        yellow: '#FFEF9F',
+        red: '#FFD6E0',
+        green : '#C1FBA4',
+        blue : '#90F1EF',
+      }
+    },
+  },
+  plugins: [],
+}
